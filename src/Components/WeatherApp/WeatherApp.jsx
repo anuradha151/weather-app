82d8767d1257ca5398d025f6e163309ecc6e0827
anuradha <anuradha@arimaclanka.com>
2023-09-30 08:37:02 +0530

    Fetch API implemented

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,21 +11,38 @@ import wind_icon from "../Assets/wind.png";
 import humidity_icon from "../Assets/humidity.png";
 
 export const WeatherApp = () => {
-  let api_key = "78dd99c05b19bda9c237d35b935b4e9a";
+  let api_key = "601f502fe4e0d133f5692e8a43b090aa";
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
 
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
 
-  const search = () => {
-        const element = document.getElementsByClassName("cityInput")
-        if(element[0].value === ""){
-            return 0;
-        }
-  }
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temprature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+
+    humidity[0].innerHTML = data.main.humidity +  " %";
+    wind[0].innerHTML = data.wind.speed + " km/h";
+    temprature[0].innerHTML = data.main.temp+ "°c";
+    location[0].innerHTML = data.name;
+  };
 
   return (
     <div className="container">
       <div className="top-bar">
         <input type="text" className="cityInput" placeholder="Search" />
-        <div className="search-icon" onClick={()=>{search()}}>
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
           <img src={search_icon} alt="" />
         </div>
       </div>
@@ -45,7 +62,7 @@ export const WeatherApp = () => {
         <div className="element">
           <img src={wind_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percent">18 km/h</div>
+            <div className="wind-rate">18 km/h</div>
             <div className="text">Wind Speed</div>
           </div>
         </div>
